b5e816f1411857421ab1f96b4a2f584db4090261
Bailey <Bailey1995@gmail.com>
2018-05-03 13:52:01 -0700

    print with parentheses

--- upload_file.py
+++ upload_file.py
@@ -9,18 +9,18 @@ def extractRelevantData(data):
 
 # helper function to print scan details in the correct format
 def formatScanDetails(data, file):
-    print ''
-    print 'filename: ' + file
-    print 'overall_status: ' + (data['scan_results']['scan_all_result_a'] if data['scan_results']['scan_all_result_a'] != 'No threat detected' else 'Clean')
+    print ('')
+    print ('filename: ' + file)
+    print ('overall_status: ' + (data['scan_results']['scan_all_result_a'] if data['scan_results']['scan_all_result_a'] != 'No threat detected' else 'Clean'))
 
     scan_details = data['scan_results']['scan_details']
     for key, value in scan_details.items():
         scan_detail = scan_details[key]
-        print ''
-        print 'engine: ' + key
-        print 'threat_found: ' + (scan_detail['threat_found'] if scan_detail['threat_found'] else 'Clean')
-        print 'scan_result: {0}'.format(scan_detail['scan_result_i'])
-        print 'def_time: ' + scan_detail['def_time']
+        print ('')
+        print ('engine: ' + key)
+        print ('threat_found: ' + (scan_detail['threat_found'] if scan_detail['threat_found'] else 'Clean'))
+        print ('scan_result: {0}'.format(scan_detail['scan_result_i']))
+        print ('def_time: ' + scan_detail['def_time'])
 
 
 import commands
@@ -34,7 +34,7 @@ md5 = hashlib.md5() # used to calculate md5 hash
 # check if a file was passed in as argument
 # program terminates if no file was passed
 if len(sys.argv) <= 2:
-    print "Please pass in an apikey and a file ex: \n     python test.py apikey1234567890 someFile.txt"
+    print ("Please pass in an apikey and a file ex: \n     python test.py apikey1234567890 someFile.txt")
     sys.exit()
 
 apikey = sys.argv[1] # apiKey from argument passed in
@@ -83,5 +83,5 @@ else:
     # print out the scan results
     formatScanDetails(j, sys.argv[2])
 
-print ""
-print 'End'
+print ("")
+print ('End')
